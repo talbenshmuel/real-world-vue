5838eca4e1d720bc5ba2dc7695fa97e2808e53f9
talbenshmuel <talbenshmuel@gmail.com>
2021-06-12 22:11:11 +0300

    before build

---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <div class="flex-center">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+import EventService from '@/services/EventsService.js';
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data(){
+    return {
+      events: null
+    }
+  },
+  created(){
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+        console.log('events:', response.data);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+};
+</script>
+
